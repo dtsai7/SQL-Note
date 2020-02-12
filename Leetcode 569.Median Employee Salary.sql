63c5bdcbefe6bb4586d69cda5752b410990cfb50
David Tsai <32302812+dtsai7@users.noreply.github.com>
2020-02-12 20:22:21 +0800

    Update Leetcode 569.Median Employee Salary.sql

--- Leetcode 569.Median Employee Salary.sql
+++ Leetcode 569.Median Employee Salary.sql
@@ -1,4 +1,5 @@
 SELECT id, Company, Salary
 FROM Employee e
-WHERE 1 >= ABS((SELECT COUNT(*) FROM Employee e1 WHERE e.Company = e1.Company AND e.Salary >= e1.Salary) = (SELECT COUNT(*) FROM Employee e2 WHERE e.Company = e2.Company AND e.Salary <= e2.Salary))
+WHERE 1 >= ABS((SELECT COUNT(*) FROM Employee e1 WHERE e.Company = e1.Company AND e.Salary >= e1.Salary) = 
+(SELECT COUNT(*) FROM Employee e2 WHERE e.Company = e2.Company AND e.Salary <= e2.Salary))
 GROUP BY Company, Salary
